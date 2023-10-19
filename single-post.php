facaa6e6b9817122b553f9c63bfbceb02d891324
Oleg <31633507+OlegSand@users.noreply.github.com>
2023-10-19 13:07:54 +0200

    Update single-post.php

--- single-post.php
+++ single-post.php
@@ -8,7 +8,6 @@
         
         <div class="news-wrapp container">
             <div class="news-card justify">
-                <!-- <div class="news-card__name"></div> -->
                 <p class="news-card__text"><?php the_content(); ?></p>
             </div>
         </div>
