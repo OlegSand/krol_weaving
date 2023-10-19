c1471798ef103a90dd212537b1de717d6db8b935
Oleg <31633507+OlegSand@users.noreply.github.com>
2023-10-19 11:08:16 +0200

    Update news.php

--- page-template/news.php
+++ page-template/news.php
@@ -1,12 +1,11 @@
 
 <?php
 /*
-    Template Name: Новости
+    Template Name: mews
 */
 ?>
 
 <?php get_header('page'); ?>
-
 
 <main class="main news">
         
@@ -15,7 +14,7 @@
         </div>
         <div class="news-wrapp container">
             <a href="<?php the_permalink(); ?>"></a>
-        <!-- перебор постов в цикле и вывод из категории 2 т. е. рубрика news -->
+        <!-- view posts -->
             <?php
                 global $post;
                 $args = array('category' => 2 );
@@ -25,26 +24,20 @@
                     ?>
                     <a href="<?php the_permalink(); ?>" class="short_description">
                         <div class="news-card">
-                                <!-- <img src="" alt="card"> -->
-                                <?php the_post_thumbnail(); ?> <!--- выводим картинку поста ---> 
-                                <div class="news-card__name"><?php the_title(); ?><!--- выводим наименование(заголовок) поста ---> 
+                                <?php the_post_thumbnail(); ?> <!--- post img ---> 
+                                <div class="news-card__name"><?php the_title(); ?><!--- post name ---> 
                                 </div>
                                 <p class="news-card__text ">
                                     <?php the_excerpt(); ?>
                                 </p>
-                                <!--- выводим краткое описание поста ---> 
+                                <!--- post short descr ---> 
                             </div>
-                            </a>
-                    <?php 
-                }
+                    </a>
+                <?php 
                 wp_reset_postdata();
                 ?>
-            
-            <!-- card end -->
         </div>
         
 </main>
-<!--         
-         -->
 
 <?php get_footer(); ?>
