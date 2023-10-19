895bd2a53ee524b4702a755c86db75653ccb1839
Oleg <31633507+OlegSand@users.noreply.github.com>
2023-10-19 13:07:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-    <!-- INDEX.PHP -->
+ 
 <main class="main news">
         <div class="news-name categoty-name">
             <p>Новини</p>
@@ -7,7 +7,6 @@
         
         <div class="news-wrapp container">
             <a href="<?php the_permalink(); ?>"></a>
-        <!-- перебор постов в цикле и вывод из категории 2 т. е. рубрика news -->
             <?php
                 global $post;
                 $args = array('category' => 2 );
@@ -16,21 +15,17 @@
                     setup_postdata($post);
                     ?>
                         <div class="news-card">
-                                <!-- <img src="" alt="card"> -->
-                                <?php the_post_thumbnail(); ?> <!--- выводим картинку поста ---> 
-                                <div class="news-card__name"><?php the_title(); ?><!--- выводим наименование(заголовок) поста ---> 
+                                <?php the_post_thumbnail(); ?> 
+                                <div class="news-card__name"><?php the_title(); ?>
                                 </div>
                                 <p class="news-card__text">
                                     <?php the_excerpt(); ?>
                                 </p>
-                                <!--- выводим краткое описание поста ---> 
                             </div>
                     <?php 
                 }
                 wp_reset_postdata();
                 ?>
-            
-            <!-- card end -->
         </div>
     </main>
 <?php ?>
