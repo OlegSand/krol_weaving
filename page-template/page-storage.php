300eddbed2a5617df473d49632f9601187261a60
Oleg <31633507+OlegSand@users.noreply.github.com>
2023-10-19 11:15:26 +0200

    Update and rename page-nosii.php to page-storage.php

--- page-template/page-storage.php
+++ page-template/page-storage.php
@@ -1,30 +1,20 @@
 <?php
 /*
-    Template Name: Носії
+    Template Name: nosii
     Template Post Type: page
 */
 ?>
 
 <?php get_header( 'page_news' ); ?>
 
-
-
 <main class="main news about-elem">
         
         <div class="news-name categoty-name">
             <p><?php the_title(); ?></p>
         </div>
-        <?php //the_content() ?>;
-        <!-- <div class="news-wrapp container"> -->
             <!-- card 1 start-->
             <?php get_template_part( 'post-template/post', 'childpages' )?>;
             <!-- card 1 end -->
-            
-
-        <!-- </div> -->
 </main>
-
-
-
 
 <?php get_footer(); ?>
